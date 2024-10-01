e910a735bbc67ed52f443d33e1baf33b76cdba85
NerdO <NerdO@PRAMISH>
2024-10-01 09:58:59 +0545

    Console.Writeline modified

--- Ecobite/Data/ApplicationDbContext.cs
+++ Ecobite/Data/ApplicationDbContext.cs
@@ -1,14 +1,24 @@
 ﻿
+using Ecobite.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Ecobite.Data
 {
 
-    public class ApplicationDbContext :DbContext
+    public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-        {
-            
+        { 
+
         }
+        public DbSet<UserModel> Users { get; set; }
+
+
+
     }
+            
+    
+    
 }
+
+
--- Ecobite/Models/UserModel.cs
+++ Ecobite/Models/UserModel.cs
@@ -1,16 +1,44 @@
-﻿namespace Ecobite.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Ecobite.Models
 {
+    public enum UserRole
+    {
+        Admin,
+        Donor,
+        Receiver,
+        Volunteer
+    }
+
     public class UserModel
     {
+        [Key]
         public int UserId { get; set; }
+
+        [Required]
+        [StringLength (100)]
         public required string UserName { get; set; }
 
-        public required string Password { get; set; }
-
+        [Required]
+        [EmailAddress]
         public required string Email { get; set; }
 
-        public required string Phone { get; set; }
+        [Required]
+        [DataType (DataType.Password)] 
+        public required string Password { get; set; }
+
+        [Phone]
+        public required string Phonenumber { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string? Address { get; set; }
+
+        [Required]
+        public UserRole Role { get; set; }  
          
+
 
 
     }
